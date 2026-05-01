ee26963b36f241443e238f6e3a342ee4fc72da7d
Brian Futrell <bfutrel@gmail.com>
2026-05-01 08:58:23 -0400

    module 4 - Setting up NGXS in Angular - finished

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { hide, show } from 'src/app/ngrx-state/actions/home.actions';
+import { Hide, Show } from 'src/app/state-ngxs/actions/home.actions';
 
 @Component({
   selector: 'app-home',
@@ -11,34 +12,58 @@ import { hide, show } from 'src/app/ngrx-state/actions/home.actions';
 export class HomeComponent {
 
   // isShowMore: boolean = false;
-  showMoreButtonText: string = "Show more...";
+  isShowMore$ = this.store.select(state => state.home);
 
-  show$: Observable<boolean> = new Observable();
-
-
-  
-  constructor(private store: Store<{home: false}>) {
-    this.show$ = this.store.select('home');
+  constructor(private store: Store) {
   }
 
-  // toggleShowMore(): void {
-  //   this.showMore = !this.showMore;
-
-  //   if (this.showMore) {
-  //     this.showMoreButtonText = "Show less...";
-  //   }
-  //   else {
-  //     this.showMoreButtonText = "Show more...";
-  //   }
-  // }
-
   showMore(): void {
-    this.store.dispatch(show());
+    this.store.dispatch(new Show());
   }
 
   showLess(): void {
-    this.store.dispatch(hide());
+    this.store.dispatch(new Hide());
   }
-
-
 }
+
+// ---- original code using @ngrx/store ----
+// import { Component } from '@angular/core';
+// import { Store } from '@ngrx/store';
+// import { Observable } from 'rxjs';
+// import { hide, show } from 'src/app/ngrx-state/actions/home.actions';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent {
+
+//   // isShowMore: boolean = false;
+//   showMoreButtonText: string = "Show more...";
+
+//   show$: Observable<boolean> = new Observable();
+
+//   constructor(private store: Store<{home: false}>) {
+//     this.show$ = this.store.select('home');
+//   }
+
+//   // toggleShowMore(): void {
+//   //   this.showMore = !this.showMore;
+
+//   //   if (this.showMore) {
+//   //     this.showMoreButtonText = "Show less...";
+//   //   }
+//   //   else {
+//   //     this.showMoreButtonText = "Show more...";
+//   //   }
+//   // }
+
+//   showMore(): void {
+//     this.store.dispatch(show());
+//   }
+
+//   showLess(): void {
+//     this.store.dispatch(hide());
+//   }
+// }
