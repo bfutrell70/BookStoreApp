c74ab97ae9dd8c7ddf78c21081af0609a8da88ea
Brian Futrell <bfutrel@gmail.com>
2026-05-01 10:21:12 -0400

    added @Injectable decorator

--- src/app/state-ngxs/state/home.state.ts
+++ src/app/state-ngxs/state/home.state.ts
@@ -1,10 +1,12 @@
 import { Action, State, StateContext } from "@ngxs/store";
 import { Hide, Show } from "../actions/home.actions";
+import { Injectable } from "@angular/core";
 
 @State<boolean>({
     name: 'home',
     defaults: false
 })
+@Injectable()
 export class HomeState {
     @Action(Show)
     show(ctx: StateContext<boolean>) {
